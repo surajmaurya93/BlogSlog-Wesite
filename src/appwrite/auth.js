bf65328b5776333afae1b06f872bd36e9eb08d80
Suraj Maurya <surajmaurya0706@gmail.com>
2025-01-27 14:21:53 +0530

    megablog

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -11,7 +11,6 @@ export class AuthService {
             .setEndpoint(conf.appwriteUrl)
             .setProject(conf.appwriteProjectId);
         this.account = new Account(this.client);
-
     }
 
     async createAccount({ email, password, name }) {
@@ -30,7 +29,7 @@ export class AuthService {
 
     async login({ email, password }) {
         try {
-            return await this.account.createEmailSession(email, password);
+            return await this.account.createEmailPasswordSession(email, password);
         } catch (error) {
             throw error;
         }
